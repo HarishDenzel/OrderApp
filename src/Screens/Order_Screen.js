f8c02c6e6c36f84fd0e95bdacf86e5a88130a219
User0939 <>
2022-12-23 10:25:45 +0530

    order screen

--- src/Screens/Order_Screen.js
+++ src/Screens/Order_Screen.js
@@ -1,10 +1,19 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View ,ScrollView,Image} from 'react-native'
 import React from 'react'
-
+import metrics from '../Utils/mertrics'
 export default function Order_Screen() {
   return (
     <View style={{flex:1,backgroundColor:'green'}}>
-      <Text>Order_Screen</Text>
+      <View style={{flex:0.92}}>
+<ScrollView style={{flex:1}}>
+<Image source={require("../Assets/bg.png")} style={{height:metrics.screenHeight/100*30,width:metrics.screenWidth/100*100}} />
+
+</ScrollView>
+      </View>
+      <View style={{flex:0.08,backgroundColor:"red"}}>
+        
+      </View>
+     
     </View>
   )
 }
